b61ee50987ab98e407e04b82d07c130d573686ac
Slashh48 <pablodiablofun@gmail.com>
2023-08-23 15:18:01 +0200

    Fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
 
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+  //slug! - Kada se doda uzvicnik na kraju, zajebavamo Typescript, u tom smislu da to zapravo znaci da ova varijabla nikad nece biti null
+
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
