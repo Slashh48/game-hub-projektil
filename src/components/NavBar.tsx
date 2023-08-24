9252327376e38182a03bfb10c242de8af0efc197
Slashh48 <pablodiablofun@gmail.com>
2023-08-24 20:51:20 +0200

    Logo change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/game-hub-projektil-logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBox from "./SearchBox";
 import { Link } from "react-router-dom";
